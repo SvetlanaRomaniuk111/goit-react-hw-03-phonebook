da57d7bdaedf823702a8aa89f1934ce6412f465a
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-09-22 12:45:45 +0300

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import FormCreateContact from './components/Forms/CreateContact/CreateContact';
 import FormFilterContact from './components/Forms/Filter/Filter';
 import css from './components/ContactList/ContactList.module.css';
 
+const initialState = {
+  contact: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filteredContact: null,
+};
+
 class App extends Component {
-  state = {
-    contact: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filteredContact: null,
-  };
+  state = JSON.parse(localStorage.getItem('state')) || initialState;
+
+  componentDidUpdate() {
+    localStorage.getItem('state', JSON.stringify(this.state));
+  }
+
   handleDelete = id => {
     this.setState(prev => ({
       contact: prev.contact.filter(el => el.id !== id),
